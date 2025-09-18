9e54d163a3c9a498c1b39920767b69aa99859496
chayan-mann <chayanmann09@gmail.com>
2025-09-18 15:37:48 +0530

    env added in frontend

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:8000"
+export const API_BASE_URL = import.meta.env.VITE_BACKEND_URL
 
 export const API_ENDPOINTS = {
   upload: `${API_BASE_URL}/upload`,
